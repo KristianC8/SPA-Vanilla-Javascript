9100c037ab83d8940cd1cec9600352f57840e7b0
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-01-04 13:37:35 -0500

    corección grid, se agrego: estilos cards, seccion trailers

--- app/components/GameCard.js
+++ app/components/GameCard.js
@@ -2,7 +2,7 @@ export function GameCard() {
     return ` 
         <div class="game-card">
         <img src="" alt="">
-        <h2>Title</h2>
+        <h3>Title</h3>
         <span ></span>
         </div>
     `
--- app/components/Home.js
+++ app/components/Home.js
@@ -4,15 +4,44 @@ export function Home() {
     <section class="home-best-games">
     <h2>MEJOR CALIFICADOS</h2>
     <div class="home-grid-games">
-    <div class="card-1 cards"></div>
-    <div class="card-2 cards"></div>
-    <div class="home-cards cards"></div>
-    <div class="home-cards cards"></div>
-    <div class="home-cards cards"></div>
+    <div class="card-1 cards">
+    <img class="card-img-1" src="" alt="">
+    <h3 class="card-name-1">name</h3>
+    <span class="card-note-1">90</span>
+    </div>
+    <div class="home-cards cards">
+    <img class="card-img" src="" alt="">
+    <div class="card-info">
+    <h3 class="card-name">name</h3>
+    <span class="card-note">90</span>
+    </div>
+    </div>
+    <div class="home-cards cards">
+    <img class="card-img" src="" alt="">
+    <div class="card-info">
+    <h3 class="card-name">name</h3>
+    <span class="card-note">89</span>
+    </div>
+    </div>
+    <div class="home-cards cards">
+    <img class="card-img" src="" alt="">
+    <div class="card-info">
+    <h3 class="card-name">name</h3>
+    <span class="card-note">89</span>
+    </div>
+    </div>
+    <div class="home-cards cards">
+    <img class="card-img" src="" alt="">
+    <div class="card-info">
+    <h3 class="card-name">name</h3>
+    <span class="card-note">89</span>
+    </div>
+    </div>
     </div>
     </section>
     <section class="home-trailers">
     <h2>TRAILERS DESTACADOS</h2>
+    <div class="home-trailers-content"></div>
     </section>
 </section>    
 
