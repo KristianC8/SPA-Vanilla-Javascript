4fd1644e8df9eb753222c8f2f5c3a9c87536f9a9
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-02-23 15:27:58 -0500

    version funcional se corrige error screenshots mobile

--- app/index.js
+++ app/index.js
@@ -31,4 +31,21 @@ document.addEventListener("DOMContentLoaded", e => {
 window.addEventListener("hashchange", e => {
     // api.page = 1
     App()
+    const menu = localStorage.getItem('sideMenu')
+
+    if (!menu || menu === 'closed') {
+        document.querySelector('.game-screenshots-content').classList.add('closed')
+        document.querySelector('.game-screenshots-contentM').classList.add('closed')
+        document.querySelector('.game-screenshots-content').classList.remove('open')
+        document.querySelector('.game-screenshots-contentM').classList.remove('open')
+        document.querySelector('.game-screenshots-flex').classList.add('closed')
+        document.querySelector('.game-screenshots-flex').classList.remove('open')
+    } else {
+        document.querySelector('.game-screenshots-content').classList.add('open')
+        document.querySelector('.game-screenshots-contentM').classList.add('open')
+        document.querySelector('.game-screenshots-content').classList.remove('closed')
+        document.querySelector('.game-screenshots-contentM').classList.remove('closed')
+        document.querySelector('.game-screenshots-flex').classList.add('open')
+        document.querySelector('.game-screenshots-flex').classList.remove('closed')
+    }
 })
