4cfd4c5625a88c80498ad82cd07de4b6dc5ebafd
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-08-03 15:37:54 -0500

    fix mobile menu measures

--- app/components/MobileMenu.js
+++ app/components/MobileMenu.js
@@ -22,10 +22,10 @@ export function MobileMenu() {
 
     //Validar que si el estado del menu es cerrado se aplique el clipath se hace cuando se recarga la pagina con el cambio de #
     if (!stateMenu && isNavigationBarVisible()) {
-        $MobileMenu.style.clipPath = `circle(1rem at calc(100vw - 34rem) calc(${window.innerHeight}rem - 34rem))`
+        $MobileMenu.style.clipPath = `circle(.0625rem at calc(100vw - 2.125rem) calc(${window.innerHeight}px - 2.125rem))`
     }
     else {
-        $MobileMenu.style.clipPath = `circle(1rem at calc(100vw - 34rem) calc(100vh - 34rem))`
+        $MobileMenu.style.clipPath = `circle(.0625rem at calc(100vw - 2.125rem) calc(100vh - 2.125rem))`
     }
 
 
@@ -33,10 +33,10 @@ export function MobileMenu() {
     function updateStatus() {
         // const statusElement = document.getElementById('status');
         if (isNavigationBarVisible() && !stateMenu) {
-            $MobileMenu.style.clipPath = `circle(1rem at calc(100vw - 34rem) calc(${window.innerHeight}rem - 34rem))`
+            $MobileMenu.style.clipPath = `circle(.0625rem at calc(100vw - 2.125rem) calc(${window.innerHeight}px - 2.125rem))`
             // statusElement.textContent = 'La barra del navegador es visible.';
         } else if (!isNavigationBarVisible() && !stateMenu) {
-            $MobileMenu.style.clipPath = `circle(1rem at calc(100vw - 34rem) calc(100vh - 34rem))`
+            $MobileMenu.style.clipPath = `circle(.0625rem at calc(100vw - 2.125rem) calc(100vh - 2.125rem))`
             // statusElement.textContent = 'La barra del navegador no es visible.';
         }
     }
@@ -51,8 +51,8 @@ export function MobileMenu() {
 
 
     //Evento click que aplica el clipath de cerrar el menu cuando se selecciona una opcion en el menu
-    const closeWithNav = `circle(1rem at calc(100vw - 34rem) calc(${window.innerHeight}rem - 34rem))`,
-        close = `circle(1rem at calc(100vw - 34rem) calc(100vh - 34rem))`
+    const closeWithNav = `circle(.0625rem at calc(100vw - 2.125rem) calc(${window.innerHeight}px - 2.125rem))`,
+        close = `circle(.0625rem at calc(100vw - 2.125rem) calc(100vh - 2.125rem))`
     document.addEventListener('click', e => {
         if (e.target.matches('.option-mobile-menu')) {
             if (isNavigationBarVisible()) {
@@ -101,10 +101,10 @@ export function BtnMobileMenu() {
                 $btnMobileMenu.classList.remove('rubberBand');
             }, 600);
 
-            const closeWithNav = `circle(1rem at calc(100vw - 34rem) calc(${window.innerHeight}rem - 34rem))`,
-                close = `circle(1rem at calc(100vw - 34rem) calc(100vh - 34rem))`,
-                openWithNav = `circle(calc(100vh * 4) at calc(100vw - 34rem) calc(${window.innerHeight}rem - 34rem))`,
-                open = `circle(calc(100vh * 4) at calc(100vw - 34rem) calc(100vh - 34rem))`
+            const closeWithNav = `circle(.0625rem at calc(100vw - 2.125rem) calc(${window.innerHeight}px - 2.125rem))`,
+                close = `circle(.0625rem at calc(100vw - 2.125rem) calc(100vh - 2.125rem))`,
+                openWithNav = `circle(calc(100vh * 4) at calc(100vw - 2.125rem) calc(${window.innerHeight}px - 2.125rem))`,
+                open = `circle(calc(100vh * 4) at calc(100vw - 2.125rem) calc(100vh - 2.125rem))`
 
             if (stateMenu) {
                 if (isNavigationBarVisible()) {
