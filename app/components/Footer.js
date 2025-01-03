55e452a096573dc0695a29f4c09d448f97c3f518
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-01-03 17:33:29 -0500

    changed year footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,43 +1,47 @@
-
 export function Footer() {
-    const $footer = document.createElement('footer')
-    $footer.classList.add('footer')
-    $footer.classList.add('isInactive')
+  const $footer = document.createElement('footer')
+  $footer.classList.add('footer')
+  $footer.classList.add('isInactive')
 
-    let actualTheme = document.documentElement.dataset.theme
+  let actualTheme = document.documentElement.dataset.theme
+  const date = new Date()
+  const year = date.getFullYear()
 
-    const $btn_theme = document.getElementById('theme-btn')
+  const $btn_theme = document.getElementById('theme-btn')
 
-    $btn_theme.addEventListener('click', e => {
-        let theme = localStorage.getItem('theme')
+  $btn_theme.addEventListener('click', (e) => {
+    let theme = localStorage.getItem('theme')
 
-        if (theme === 'dark') {
-            document.querySelector('.footer-developedby-img').src = './app/assets/authLogoForDarkMode1.svg'
-        }
-        else {
-            document.querySelector('.footer-developedby-img').src = './app/assets/authLogoForClear.svg'
-        }
-    })
+    if (theme === 'dark') {
+      document.querySelector('.footer-developedby-img').src =
+        './app/assets/authLogoForDarkMode1.svg'
+    } else {
+      document.querySelector('.footer-developedby-img').src =
+        './app/assets/authLogoForClear.svg'
+    }
+  })
 
-    let html = `   
+  let html = `   
     <div class="footer-content">
         <div class="footer-developedby">
         <div class="footer-developedby-back">
-        <img src=${actualTheme === 'dark'
+        <img src=${
+          actualTheme === 'dark'
             ? './app/assets/authLogoForDarkMode1.svg'
-            : './app/assets/authLogoForClear.svg'} alt="Logo-Kristian-Cifuentes-Dev" class="footer-developedby-img" />
+            : './app/assets/authLogoForClear.svg'
+        } alt="Logo-Kristian-Cifuentes-Dev" class="footer-developedby-img" />
         </div>
         <span class="footer-developedby-name">Developed by <strong>KRISTIAN CIFUENTES</strong></span>
         <div class="footer-socialMedia">
         <a href="https://www.linkedin.com/in/kristian-dario-cifuentes-vera-bb24b5159/" class="footer-socialLink" rel="noopener noreferrer" target="_blank"><div class="foot-social social-in"></div></a>
         <a href="https://github.com/KristianC8/SPA-Vanilla-Javascript" class="footer-socialLink" rel="noopener noreferrer" target="_blank"><div class="foot-social social-gh"></div></a>
         </div>
-        <span class="footer-date"><strong>2024</strong></span>
+        <span class="footer-date"><strong>${year}</strong></span>
         </div>
     </div>
     `
 
-    $footer.insertAdjacentHTML('beforeend', html)
+  $footer.insertAdjacentHTML('beforeend', html)
 
-    return $footer
+  return $footer
 }
